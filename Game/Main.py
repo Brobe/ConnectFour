e4c4c9cfe219c6cda62aa724e31279b8f4c9802a
Roberto <robertofeldt@hotmail.com>
2017-04-01 23:33:06 +0200

    added constructor to player class and a playerlist in the main function.

--- Game/Main.py
+++ Game/Main.py
@@ -1,10 +1,30 @@
 import GUI
+import Player
 
 def run():
-    w = 7
-    h = 6
-    playField = [[0 for x in range(w)] for y in range(h)]
+    w = 7 #width of the playfield
+    h = 6 #height of the playfield
+    playField = [[0 for col in range(w)] for row in range(h)]
     GUI.printField(playField)
+    winner = False
+
+    players = []
+    inplayers = input("How many players will be playing?")
+    for index in range(0,inplayers):
+        playername = input("Name of player #"+str(index)+"?\n")
+        playermarker = input("What marker does "+str(playername)+" want?\n")
+        players.append(Player(playername,playermarker))
+
+    while(winner == False):
+
+    #incol = input("Where would you want to place your brick? (choose a value between 1 and " + str(w) + "\n")
+
+
+
+
+
+
+
     return
 
 
