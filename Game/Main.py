32e4718e583f6faebc1b0c0e44b29087e80e51ab
Roberto <robertofeldt@hotmail.com>
2017-04-01 23:54:21 +0200

    added getWinner and gameover

--- Game/Main.py
+++ Game/Main.py
@@ -1,21 +1,37 @@
 import GUI
-import Player
+from Player import *
+
+players = []
+
+def gameover():
+    return True
+
+def getWinner():
+    return players[0]
 
 def run():
     w = 7 #width of the playfield
     h = 6 #height of the playfield
     playField = [[0 for col in range(w)] for row in range(h)]
     GUI.printField(playField)
-    winner = False
 
-    players = []
-    inplayers = input("How many players will be playing?")
-    for index in range(0,inplayers):
+    inplayers = input("How many players will be playing?\n")
+    for index in range(0,int(inplayers)):
         playername = input("Name of player #"+str(index)+"?\n")
         playermarker = input("What marker does "+str(playername)+" want?\n")
-        players.append(Player(playername,playermarker))
+        player = Player(playername,playermarker)
+        players.append(player)
 
-    while(winner == False):
+    playerturn = 0
+    while(True):
+        if(playerturn == len(players)):
+            playerturn = 0
+
+        if(gameover()):
+            break
+        playerturn+=1
+
+    print("Player " + getWinner().name + " wins!")
 
     #incol = input("Where would you want to place your brick? (choose a value between 1 and " + str(w) + "\n")
 
